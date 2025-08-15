e194b7c43f094d4fbcbd61d6f93ce8d91dbd2e31
Robb Young <164847103+TheMortalSea@users.noreply.github.com>
2025-08-15 14:50:38 +0100

    changed base epoch default for testing

--- pretrain.py
+++ pretrain.py
@@ -170,7 +170,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--batch_size', type=int, default=64)
-    parser.add_argument('--epochs', type=int, default=100)
+    parser.add_argument('--epochs', type=int, default=20)
     parser.add_argument('--num_workers', type=int, default=4)
     parser.add_argument('--embed_size', type=int, default=128)
     parser.add_argument('--cityembed_size', type=int, default=4)
