73ed04c766bff34a0e2b4339ed559080216be403
Robb Young <164847103+TheMortalSea@users.noreply.github.com>
2025-08-18 12:18:33 +0100

    Update validation.py

--- validation.py
+++ validation.py
@@ -75,8 +75,6 @@ def Validation(args):
                 data['input_y'] = data['input_y'].to(device)
                 data['time_delta'] = data['time_delta'].to(device)
                 data['city'] = data['city'].to(device)
-                data['label_x'] = data['label_x'].to(device)
-                data['label_y'] = data['label_y'].to(device)
                 data['len'] = data['len'].to(device)
                 
                 # 获取推测，并将标签堆叠成张量
